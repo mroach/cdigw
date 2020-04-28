5687bec744af98bf2f3b06d38c3de2a2e6e1c10d
Michael Roach <git@c.mroach.com>
2020-04-28 23:25:13 +0200

    add a couple sanity checks

--- src/lib/cdigw_web/mscd_plug.ex
+++ src/lib/cdigw_web/mscd_plug.ex
@@ -7,7 +7,7 @@ defmodule CdigwWeb.MscdPlug do
   def call(conn, _opts) do
     conn = fetch_query_params(conn)
 
-    toc = Mscd.RequestParser.parse_query(conn.query_params["cd"])
+    {:ok, toc} = Mscd.RequestParser.parse_query(conn.query_params["cd"])
     mb_disc_id = MusicBrainz.calculate_disc_id(toc.lead_out_lba, toc.track_lbas)
     {:ok, %{"releases" => [release | _]}} = MusicBrainz.get_releases_by_disc_id(mb_disc_id)
     disc = MusicBrainz.release_to_disc(nil, release)
